99ed930434ab3368d8969da805e2259004d9d020
ktaylor99 <132228170+ktaylor99@users.noreply.github.com>
2023-05-30 13:41:43 -0500

    Accidentally broke something

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ let countEl = document.getElementById("count-el")
 let count = 0
 
 function increment(){
-    count = count + 1 
-    countEl.innerText = count
+   count += 1 
+   countEl.innerText = count
+}
+
+let SaveEl = document.getElementById("save-el")
+
+function save(){
+    count += " - "
+    SaveEl.innerText += count
 }
